66ac3abcca523cd3a663acbdd6b726a93d63b5f4
prajwalharkare <prajwal.harkare@xecurify.com>
2026-04-27 00:54:31 +0530

    fix: remove deleted device_token import

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,7 +1,6 @@
-from app.models.user import User
+from app.models.user import User, UserFCMToken
 from app.models.plan import Plan
 from app.models.notification import Notification
-from app.models.device_token import UserDeviceToken
 from app.models.other import Room, Community, CommunityMember, Conversation, ConversationMember, Message
 
 class base:
